bc8cc3441fed57267ed1082afd481bcda18ea91f
guyzamir2@gmail.com <12guitar12!@>
2019-11-25 13:48:26 +0200

    Guy

--- app/src/main/java/com/guy/inventory/InventoryApp.java
+++ app/src/main/java/com/guy/inventory/InventoryApp.java
@@ -14,8 +14,8 @@ public class InventoryApp extends Application {
     public static List<Buy> buys;
     public static List<Sale> sales;
     public static List<Export> exports;
-    public static final String APPLICATION_ID = "C9CFD411-6AA7-87D4-FF53-EF56C4CC4B00";
-    public static final String API_KEY = "E12C2F9C-6B3B-E8B4-FFD5-FC3420C35E00";
+    public static final String APPLICATION_ID = "Enter your Application ID here";
+    public static final String API_KEY = "Enter your API key here";
 
     @Override
     public void onCreate() {
